d2876348f35aff0e56852479375f30a1f8e42aeb
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-04-02 14:44:30 +0800

    Update OpenWrt-x86-64.sh

--- OpenWrt-x86-64.sh
+++ OpenWrt-x86-64.sh
@@ -19,8 +19,10 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
 
 # 自定义定制选项
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate #定制默认IP
-sed -i 's#stats auth admin:root#stats auth :#g' package/lean/luci-app-haproxy-tcp/root/etc/haproxy_init.sh #去除haproxy默认密码
+sed -i 's#192.168.1.1#10.0.0.1#g' package/base-files/files/bin/config_generate #定制默认IP
+sed -i 's@stats auth admin:root@#stats auth admin:root@g' package/lean/luci-app-haproxy-tcp/root/etc/haproxy_init.sh #去除haproxy默认密码
+sed -i 's#frontend ss-in#frontend ss-HAProxy#g' package/lean/luci-app-haproxy-tcp/root/etc/haproxy_init.sh #修改haproxy默认节点名称
+sed -i 's#backend ss-out#backend HAProxy-out#g' package/lean/luci-app-haproxy-tcp/root/etc/haproxy_init.sh #修改haproxy默认节点名称
 sed -i 's#option commit_interval 24h#option commit_interval 10m#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计写入为10分钟
 sed -i 's#option database_directory /var/lib/nlbwmon#option database_directory /etc/config/nlbwmon_data#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计数据存放默认位置
 
