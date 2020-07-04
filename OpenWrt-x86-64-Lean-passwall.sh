83f67075dff4e703c2689aef7bbfec754e40efca
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-07-04 18:30:04 +0800

    更新

--- OpenWrt-x86-64-Lean-passwall.sh
+++ OpenWrt-x86-64-Lean-passwall.sh
@@ -16,7 +16,7 @@ cat feeds.conf.default
 
 # 添加第三方软件包
 git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-git clone https://github.com/db-one/small package/small
+git clone https://github.com/kenzok8/small package/small
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 
