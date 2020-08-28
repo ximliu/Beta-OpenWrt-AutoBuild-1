84bf38a34d508f64a7d65f65ddce20bc475ae096
db-one <20243226+kang-mk@users.noreply.github.com>
2020-08-28 11:30:15 +0800

    Update config

--- OpenWrt-x86-64-Lienol.sh
+++ OpenWrt-x86-64-Lienol.sh
@@ -54,7 +54,7 @@ touch ./.config
 # 有些插件/选项是默认开启的, 如果想要关闭, 请参照以下示例进行编写:
 # 
 #          =========================================
-#         |  # 取消编译VMware镜像:                    |
+#         |  # 取消编译VMware镜像:                   |
 #         |  cat >> .config <<EOF                   |
 #         |  # CONFIG_VMDK_IMAGES is not set        |
 #         |  EOF                                    |
@@ -189,9 +189,6 @@ CONFIG_PACKAGE_luci-app-control-webrestriction=y #访问限制
 CONFIG_PACKAGE_luci-app-control-weburl=y #网址过滤
 CONFIG_PACKAGE_luci-app-nlbwmon=y #宽带流量监控
 CONFIG_PACKAGE_luci-app-wrtbwmon=y #实时流量监测
-CONFIG_PACKAGE_luci-app-statistics=y
-CONFIG_PACKAGE_luci-app-vnstat=y
-CONFIG_PACKAGE_luci-app-watchcat=y
 CONFIG_PACKAGE_luci-app-sfe=y #高通开源的 Shortcut FE 转发加速引擎
 # CONFIG_PACKAGE_luci-app-smartdns is not set #smartdns服务器
 # CONFIG_PACKAGE_luci-app-flowoffload is not set #开源 Linux Flow Offload 驱动
